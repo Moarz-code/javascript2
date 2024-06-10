71059ee45d539378c9d806302905214a1f53fd1b
Moarz Naing Lin Khant <nainglinkhant2005@gmail.com>
2024-06-10 09:21:43 +0800

    Basic String manipulation

---
+++ 1 strings.js
@@ -0,0 +1,24 @@
+const name = 'John'; 
+const age = 30;
+
+// Concatenation 
+
+console.log("My name is " + name + " and my age is " + age)
+console.log(`My name is ${name} and I am ${age} years old`)
+
+//length
+const s = "Hello World"
+console.log(s.length) //this is a property 
+// as it is a property it doesn't have a () 
+
+
+console.log(s.substring(0,5));// mid function ? 
+console.log(s.substring(0,5).toUpperCase()); //can stack up 
+
+
+//To convert into arrays
+console.log(s.split(''));
+const brands = "Samsung, Apple, Lenovo"
+brands.split(",")
+console.log(brands)// notice here you still get the string 
+console.log(brands.split(","))// here you get the the array 
--- one.js
+++ one.js
@@ -1,11 +1,10 @@
-alert('hello world');
+const name = "Moarz"; 
+const age = 18; 
+const rating = 7.8; //there's no float or integer only num
+const isMale = true; //notice how true is in all small letters unlike True in python
+const x = null; 
+const y = undefined; 
+let z; // this is also undefined 
 
-console.log("Hi")
-console.error("This is an error")
-console.warn("this is a warning")
 
-
-const age = 30; 
-console.log(age);
-
-age = 31;
+console.log(typeof y); 
